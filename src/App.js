353c9b586f0a8a98d1414b03d364d53f1b907f54
Mohamed-A-Badr <badr49960@gmail.com>
2022-08-25 19:00:18 +0200

    Project2-phase1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import Carousel from "./Carousel";
 import Course from "./Course";
 import courses from "./Courses_content.json";
 console.log(courses);
 
 function App() {
   return (
-    <Carousel
-      courses={courses.Courses.Python}
-      autoplay={true}
-      delay={10}
-      courseWidth={"18rem"}
-      courseHeight={"auto"}
-      courseMargin={10}
-    />
+    <div className="courses-container">
+      {courses.Courses.Python.map((course) => (
+        <Course course={course} />
+      ))}
+    </div>
   );
 }
 
